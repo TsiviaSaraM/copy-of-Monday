794cd7bed1bbd48398ba0067e6a280fad0d7a1cb
TsiviaSaraM <tsiviasara@gmail.com>
2022-01-04 15:11:23 +0000

    worked on BoardAside

--- src/cmps/BoardAside.jsx
+++ src/cmps/BoardAside.jsx
@@ -7,23 +7,25 @@ import { useOnClickOutside } from '../hooks/useOnClickOutside';
 import { useState } from 'react';
 import { FormBoardAsideControls } from './forms/FormBoardAsideControls';
 import { GoKebabHorizontal } from 'react-icons/go'
+import { useSelector } from 'react-redux';
 
 //board CRUD is here
 export const BoardAside = ({ boards, onAddBoard, onRemoveBoard, onEditBoard, onSelectBoard, editBoardFilter, onInsertBoard }) => {
 
-    
+
     const [addBoardFormOpen, setAddBoardFormOpen] = useState(false)
     const [controlsFormOpen, setControlsFormOpen] = useState(false)
     const [boardToControl, setBoardToControl] = useState(null)
     const [boardFilter, setBoardFilter] = useState(null)
     const ref = useRef()
+    const { loggedInUser } = useSelector(state => state.userModule) || { fullname: 'mister user' }
     useOnClickOutside(ref, () => {
         setAddBoardFormOpen(false)
         setControlsFormOpen(false)
     })
 
     const insertNewBoard = (direction, activeBoard) => {
-        const difference  = direction === 'ABOVE' ? 0 : 1
+        const difference = direction === 'ABOVE' ? 0 : 1
         const position = boards.findIndex(board => board._id === activeBoard._id) + difference
         onInsertBoard(position)
     }
@@ -38,9 +40,9 @@ export const BoardAside = ({ boards, onAddBoard, onRemoveBoard, onEditBoard, onS
         const field = event.target.name
         const value = event.target.value
         // setBoardFilter({[field]: value})
-        setBoardFilter(boardFilter => {return {...boardFilter, [field]: value}});
+        setBoardFilter(boardFilter => { return { ...boardFilter, [field]: value } });
         // dispatch(setFilterBy())
-        editBoardFilter({[field]: value})
+        editBoardFilter({ [field]: value })
     }
 
     // setControlsFormOpen(true)
@@ -65,7 +67,26 @@ export const BoardAside = ({ boards, onAddBoard, onRemoveBoard, onEditBoard, onS
                     </div>
                 </>
             }
-            <h1>Boards</h1>
+            <div className="header">
+                <div className="user">
+                    <span>{loggedInUser.fullname}</span>
+                </div>
+
+                {/* NB separate workspaces is a feature that will come later */}
+                <div className="workspace">
+                    <h2 className="workspace-title-container" >
+                        <div className="letter-container">
+                            <span>M</span>
+                            <div className="icon-container">
+                                <i></i>
+                            </div>
+                        </div>
+                        <div className="workspace-title">My workspace</div>
+                    </h2>
+                </div>
+
+            </div>
+
             <li className="board-control-container" onClick={() => setAddBoardFormOpen(true)}>
                 <div className="board-control flex">
                     <svg viewBox="0 0 20 20" fill="currentColor" width="19" height="19" role="img" aria-hidden="true" className="icon_component icon_component--no-focus-style"><path d="M10.75 3C10.75 2.58579 10.4142 2.25 10 2.25C9.58579 2.25 9.25 2.58579 9.25 3V9.25H3C2.58579 9.25 2.25 9.58579 2.25 10C2.25 10.4142 2.58579 10.75 3 10.75H9.25V17C9.25 17.4142 9.58579 17.75 10 17.75C10.4142 17.75 10.75 17.4142 10.75 17V10.75H17C17.4142 10.75 17.75 10.4142 17.75 10C17.75 9.58579 17.4142 9.25 17 9.25H10.75V3Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
@@ -98,7 +119,7 @@ export const BoardAside = ({ boards, onAddBoard, onRemoveBoard, onEditBoard, onS
                             <p className="title" onClick={() => onSelectBoard(board)} >{board.title} </p>
                             {/* <svg onClick={() => onEditBoardName(board)} viewBox="0 0 20 20" fill="currentColor" width="16" height="16" role="img" aria-hidden="true" aria-label="Rename Board" className="edit-board icon_component icon_component--no-focus-style"><path d="M13.8542 3.59561C13.8541 3.59568 13.8542 3.59555 13.8542 3.59561L4.80915 12.6503L3.81363 16.189L7.35682 15.1957L16.4018 6.14C16.4746 6.06722 16.5161 5.96795 16.5161 5.86503C16.5161 5.76221 16.4753 5.6636 16.4026 5.59083C16.4025 5.59076 16.4026 5.59091 16.4026 5.59083L14.4038 3.59568C14.3309 3.52292 14.232 3.48197 14.1289 3.48197C14.026 3.48197 13.927 3.52297 13.8542 3.59561ZM12.8051 2.54754C13.1562 2.19695 13.6324 2 14.1289 2C14.6254 2 15.1016 2.19693 15.4527 2.54747C15.4527 2.5475 15.4527 2.54745 15.4527 2.54747L17.4515 4.54263C17.8026 4.89333 18 5.36914 18 5.86503C18 6.36091 17.8028 6.8365 17.4518 7.18719L8.26993 16.3799C8.17984 16.4701 8.06798 16.5356 7.94516 16.57L2.94244 17.9724C2.68418 18.0448 2.4069 17.9723 2.21725 17.7829C2.0276 17.5934 1.95512 17.3165 2.02768 17.0586L3.43296 12.0633C3.46728 11.9413 3.53237 11.8301 3.62199 11.7404L12.8051 2.54754Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg> */}
                             {/* <p onClick={() => onRemoveBoard(board)}>x</p> */}
-                            <div className="kebab" onClick={()=>setControlsFormOpen(true)}><GoKebabHorizontal /></div>
+                            <div className="kebab" onClick={() => setControlsFormOpen(true)}><GoKebabHorizontal /></div>
 
                         </div>
                     </li>
